56b773662405a831e0ea9a5c8e3a19968e64a2d6
Marcel <123helpmestudy@gmail.com>
2022-10-09 18:38:36 +0100

    function for getting battery stats

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 import os
+import time
+
+def main():
+    """Main function to check battery status"""
+    os.system("pi-top battery > battery.txt")
+    # Read in the contents of the text file
+    contents = None
+    with open(os.path.join(os.getcwd(), "battery.txt"), "r") as f:
+        contents = f.read()
+    if contents is None:
+        return None
+    # Send contents in a slack message
+    time.sleep(10 * 60)
+
+
+
+if __name__ == "__main__":
+    main()
 
